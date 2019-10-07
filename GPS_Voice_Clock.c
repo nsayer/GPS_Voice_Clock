99a6f64a95588ed9da325303e77883fc8e25c7fd
Nick Sayer <nsayer@kfu.com>
2019-10-06 19:49:35 -0700

    Instead of turning the DAC off, set it to a fixed bias. This makes the ticks' volume more even.

--- GPS_Voice_Clock.c
+++ GPS_Voice_Clock.c
@@ -695,7 +695,8 @@ static unsigned char audio_poll(void) {
 		// need to wait for the other buffer before marking
 		// playback all the way done.
 		if ((EDMA.STATUS & (EDMA_CH2BUSY_bm | EDMA_CH0BUSY_bm)) == 0) {
-			DACA.CTRLA &= ~(DAC_CH0EN_bm);
+			DACA.CH0DATA = 0x7fff; // Leave it at the midpoint for bias
+			DACA.CTRLB |= DAC_CH0TRIG_bm; // send it
 			audio_playing = 0;
 		}
 		return audio_playing;
@@ -738,7 +739,6 @@ static unsigned char play_file(char *filename) {
 	if (!cnt) return 0; // empty file - we're done
 
 	audio_playing = 1;
-	DACA.CTRLA |= DAC_CH0EN_bm;
 	EDMA.CH0.TRFCNT = cnt;
 	EDMA.CH0.CTRLA |= EDMA_CH_ENABLE_bm; // start
 	if (cnt != sizeof(audio_buf[0])) {
